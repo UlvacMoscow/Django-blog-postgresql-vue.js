22a6c3503ee25153bf3865c16b96403fa285b25f
Arsenei Abraamyan <ar.abraamyan@gmail.com>
2018-10-29 21:56:48 +0300

    add class News

--- app/blog/post/models.py
+++ app/blog/post/models.py
@@ -1,3 +1,21 @@
 from django.db import models
+from django.contrib.auth import get_user_model
 
-# Create your models here.
+
+class News(models.Model):
+    """ Класс новостей
+    """
+    User = models.ForeignKey(User, verbose_name="Author")
+    title = models.CharField("title", max_length=100)
+    text_min = models.TextField("min chars", max_length=350)
+    text = models.TextField("Text news")
+    created = models.DateTimeField("Date created", auto_now_add=True)
+    description = models.CharField("Info", max_length=100)
+    keywords = models.CharField("keywords", max_length=50)
+
+    class Meta:
+        verbose_name = "News"
+        verbose_name_plural = "All news"
+
+    def __str__(self):
+        return self.title
