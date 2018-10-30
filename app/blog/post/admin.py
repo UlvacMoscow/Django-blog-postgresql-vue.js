54cdf4c33d8372f582f65d9bcc3ef2a6044f2ddf
Arsenei Abraamyan <ar.abraamyan@gmail.com>
2018-10-30 23:02:03 +0300

    добавили функционал News, Cetegory, Tag в административную панель

--- app/blog/post/admin.py
+++ app/blog/post/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from news.models import News, Category, Tags
 
+
+admin.site.register(News)
+admin.site.register(Category)
+admin.site.register(Tag)
 # Register your models here.
