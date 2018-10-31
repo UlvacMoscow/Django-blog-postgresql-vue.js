2dae6784dbad4fb5be862a9beb9e16de9af9ba19
Arsenei Abraamyan <ar.abraamyan@gmail.com>
2018-10-31 13:52:02 +0300

    создали def по выводу всех новостей

--- app/blog/post/views.py
+++ app/blog/post/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
+from post.models import News
 
-# Create your views here.
+
+
+def news_list(request):
+    """ выводит все новости
+    """
+    news = News.objects.all()
+    return render(request, "news/news_list.html", {"news": news})
