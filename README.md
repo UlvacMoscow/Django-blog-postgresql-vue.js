e1dc57e33d537bbe86b354b251d9c59013ef140f
Arsenei Abraamyan <ar.abraamyan@gmail.com>
2018-11-06 18:05:20 +0300

    подключил django-summernote

--- README.md
+++ README.md
@@ -1,3 +1,9 @@
 Стандартный учебный блог на django 2+
 используем postgresql
 используем vue.js
+
+ссылка на расширение summernote https://github.com/summernote/django-summernote
+
+Установка:
+
+pip install -r requirements.txt
--- app/blog/post/admin.py
+++ app/blog/post/admin.py
@@ -1,8 +1,13 @@
 from django.contrib import admin
 from post.models import News, Category, Tag
+from django_summernote.admin import SummernoteModelAdmin
 
 
-admin.site.register(News)
+class NewsAdmin(SummernoteModelAdmin):
+    summernote_fields = ('text', 'text_min', )
+
+
+admin.site.register(News, NewsAdmin)
 admin.site.register(Category)
 admin.site.register(Tag)
 # Register your models here.
