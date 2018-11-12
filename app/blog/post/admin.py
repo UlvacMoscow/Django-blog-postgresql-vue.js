1e7bd490788130d1a76dad29b2837e52de823ea6
Arsenei Abraamyan <ar.abraamyan@gmail.com>
2018-11-12 20:48:20 +0300

    add class Comments admin panel

--- app/blog/post/admin.py
+++ app/blog/post/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from post.models import News, Category, Tag
+from post.models import News, Category, Tag, Comments
 from django_summernote.admin import SummernoteModelAdmin
 
 
@@ -10,4 +10,6 @@ class NewsAdmin(SummernoteModelAdmin):
 admin.site.register(News, NewsAdmin)
 admin.site.register(Category)
 admin.site.register(Tag)
+
+admin.site.register(Comments)
 # Register your models here.
